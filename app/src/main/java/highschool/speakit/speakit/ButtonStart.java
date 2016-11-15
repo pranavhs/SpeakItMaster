2c5c6c249f8c115fbdd2fa278d2972f6bddf1edc
pranavhs <pranavs702@gmail.com>
2016-11-14 18:49:00 -0800

    Initial commit

---
+++ app/src/main/java/highschool/speakit/speakit/ButtonStart.java
@@ -0,0 +1,14 @@
+package highschool.speakit.speakit;
+
+/**
+ * Created by pranavandprathik on 11/11/16.
+ */
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+
+public class ButtonStart{
+    public void isClicked(){
+
+    }
+}
